a3cf7a9e238432ac0989df075b2a12fd7dd84663
kitkatMielPineda <kitkat.miel.pineda@gmail.com>
2025-06-04 11:39:09 +0800

    remove unused useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 import Footer from "@/components/Footer";
 
 export default function Home() {
